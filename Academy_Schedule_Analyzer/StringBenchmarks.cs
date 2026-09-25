a22aa936ef3ace576fc9a9570a036ebac66494fd
Jessica Wael <jessicawael68@gmail.com>
2026-09-25 03:51:49 +0300

    Add academy schedule analyzer project

---
+++ Academy_Schedule_Analyzer/StringBenchmarks.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+
+[MemoryDiagnoser]
+public class StringBenchmarks
+{
+    [Params(100, 1000, 10000, 100000)]
+    public int Iterations;
+
+    private const string Text = "x";
+
+    [Benchmark]
+    public string StringConcatenation()
+    {
+        string result = "";
+
+        for (int i = 0; i < Iterations; i++)
+        {
+            result += Text;
+        }
+
+        return result;
+    }
+
+    [Benchmark]
+    public string StringBuilderConcatenation()
+    {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < Iterations; i++)
+        {
+            result.Append(Text);
+        }
+
+        return result.ToString();
+    }
+}
